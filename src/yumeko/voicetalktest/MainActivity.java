2c981bc5ec0fe3b629d0f1184d2703d628963d50
isayamayomi <yumeko@outlook.com>
2013-06-27 17:39:23 +0800

    amr code

--- src/yumeko/voicetalktest/MainActivity.java
+++ src/yumeko/voicetalktest/MainActivity.java
@@ -20,16 +20,18 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	private static int numAudioSource 		= MediaRecorder.AudioSource.MIC;
-	private static int numSampleRateInHz 		= 44100;
-	private static int numChannelConfigIn 	= AudioFormat.CHANNEL_IN_MONO;
-	private static int numChannelConfigOut 	= AudioFormat.CHANNEL_OUT_MONO;
-	private static int numAudioFormat 		= AudioFormat.ENCODING_PCM_16BIT;
+	private static int numAudioSource = MediaRecorder.AudioSource.MIC;
+	private static int numSampleRateInHz = 44100;
+	private static int numChannelConfigIn = AudioFormat.CHANNEL_IN_MONO;
+	private static int numChannelConfigOut = AudioFormat.CHANNEL_OUT_MONO;
+	private static int numAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
+	private int numBufferSizeInBytes;
+	private AudioRecord mAudioRecode;
+	private boolean isRecording;
 	
-	private int 			numBufferSizeInBytes;
-	private AudioRecord 	mAudioRecode;
-	private boolean 		isRecording;
-	
+	private MediaRecorder mMediaRecorder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,58 +45,58 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void record(View v){
+	public void record(View v) {
 		File file = new File(Environment.getExternalStorageDirectory()
-                .getAbsolutePath() + "/test.pcm");
-		
-		if(file.exists()){
+				.getAbsolutePath() + "/test.pcm");
+
+		if (file.exists()) {
 			file.delete();
 		}
-		
-		try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to create "
-                    + file.toString());
-        }
-		
-		try{
-			final FileOutputStream fos = new FileOutputStream(file);
-			
-            numBufferSizeInBytes = AudioRecord.getMinBufferSize(
-                    numSampleRateInHz, numChannelConfigIn, numAudioFormat);
-            mAudioRecode = new AudioRecord(numAudioSource, numSampleRateInHz,
-                    numChannelConfigIn, numAudioFormat, numBufferSizeInBytes);
-            
-            final byte[] buffer = new byte[numBufferSizeInBytes];
-            
-            new Thread(){
-            	
-            	@Override
-            	public void run(){
 
-                    mAudioRecode.startRecording();
-                    isRecording = true;
-                    
-            		while (isRecording) {
-                    	mAudioRecode.read(buffer, 0, numBufferSizeInBytes);
-                        try {
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			throw new IllegalStateException("Failed to create "
+					+ file.toString());
+		}
+
+		try {
+			final FileOutputStream fos = new FileOutputStream(file);
+
+			numBufferSizeInBytes = AudioRecord.getMinBufferSize(
+					numSampleRateInHz, numChannelConfigIn, numAudioFormat);
+			mAudioRecode = new AudioRecord(numAudioSource, numSampleRateInHz,
+					numChannelConfigIn, numAudioFormat, numBufferSizeInBytes);
+
+			final byte[] buffer = new byte[numBufferSizeInBytes];
+
+			new Thread() {
+
+				@Override
+				public void run() {
+
+					mAudioRecode.startRecording();
+					isRecording = true;
+
+					while (isRecording) {
+						mAudioRecode.read(buffer, 0, numBufferSizeInBytes);
+						try {
 							fos.write(buffer);
 						} catch (IOException e) {
 							e.printStackTrace();
 							isRecording = false;
 						}
-                    }
-            		 mAudioRecode.stop();
-                     mAudioRecode.release();// 释放资源
-                     mAudioRecode = null;
-                     try {
+					}
+					mAudioRecode.stop();
+					mAudioRecode.release();// 释放资源
+					mAudioRecode = null;
+					try {
 						fos.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-            	}
-            }.start();
+				}
+			}.start();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -102,58 +104,94 @@ public class MainActivity extends Activity {
 		}
 
 	}
-	
-	public void stop(View v){
+
+	public void stop(View v) {
 		isRecording = false;
 	}
-	
-	public void play(View v){
-		 File file = new File(Environment.getExternalStorageDirectory()
-                 .getAbsolutePath() + "/test.pcm");
-         FileInputStream in = null;
-         try {
-             in = new FileInputStream(file);
-         } catch (FileNotFoundException e) {
-             // TODO Auto-generated catch block
-             e.printStackTrace();
-         }
-         
-         numBufferSizeInBytes = AudioTrack.getMinBufferSize(
-                 numSampleRateInHz, numChannelConfigOut, numAudioFormat);
-         
-         byte[] buffer = new byte[numBufferSizeInBytes];
-         
-         AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-        		 numSampleRateInHz, numChannelConfigOut, numAudioFormat,
-        		 numBufferSizeInBytes, AudioTrack.MODE_STREAM);
-         // 放音
-         audioTrack.play();
-         
-         final FileInputStream inFinal = in;
-         final byte[] bufferFinal = buffer;
-         final AudioTrack audioTrackFinal = audioTrack;
-         new Thread(){
-        	 
-        	 @Override
-        	 public void run(){
-        		 int byteread=0;
-        		 
-                 try {
-                     while ((byteread = inFinal.read(bufferFinal)) != -1) {
-                         System.out.write(bufferFinal, 0, byteread);
-                         System.out.flush();
-                         audioTrackFinal.write(bufferFinal, 0, numBufferSizeInBytes);
-                     }
-                 } catch (Exception e) {
-                     Log.e("AudioTrack", "Playback Failed");
-                 }
-                 
-                 audioTrackFinal.stop();
-                 audioTrackFinal.release();
-        	 }
-        	 
-         }.start();
-         
 
+	public void play(View v) {
+		File file = new File(Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/test.pcm");
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		numBufferSizeInBytes = AudioTrack.getMinBufferSize(numSampleRateInHz,
+				numChannelConfigOut, numAudioFormat);
+
+		byte[] buffer = new byte[numBufferSizeInBytes];
+
+		AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+				numSampleRateInHz, numChannelConfigOut, numAudioFormat,
+				numBufferSizeInBytes, AudioTrack.MODE_STREAM);
+		// 放音
+		audioTrack.play();
+
+		final FileInputStream inFinal = in;
+		final byte[] bufferFinal = buffer;
+		final AudioTrack audioTrackFinal = audioTrack;
+		new Thread() {
+
+			@Override
+			public void run() {
+				int byteread = 0;
+
+				try {
+					while ((byteread = inFinal.read(bufferFinal)) != -1) {
+						System.out.write(bufferFinal, 0, byteread);
+						System.out.flush();
+						audioTrackFinal.write(bufferFinal, 0,
+								numBufferSizeInBytes);
+					}
+				} catch (Exception e) {
+					Log.e("AudioTrack", "Playback Failed");
+				}
+
+				audioTrackFinal.stop();
+				audioTrackFinal.release();
+			}
+
+		}.start();
+	}
+	
+	private recordByAmr(){
+		mMediaRecorder = new MediaRecorder();
+
+		mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.RAW_AMR);
+		final int mono = 1;
+		mMediaRecorder.setAudioChannels(mono);
+		mMediaRecorder.setAudioSamplingRate(8000);
+		mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		mMediaRecorder.setOutputFile(Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/test.amr");
+		
+		new Thread(){
+			
+		}.start();
+	}
+	
+	public void talk(View v){
+		
+	}
+	
+	private void initLocalSocket(){
+		
+	}
+	
+	private void prepareAudioRecoder(){
+		
+	}
+	
+	private void sendDate(){
+		
+	}
+	
+	private void readAmrFrame(){
+		
 	}
 }
---
+++ src/yumeko/voicetalktest/MediaRecoderTest.java
@@ -0,0 +1,7 @@
+package yumeko.voicetalktest;
+
+import android.media.MediaRecorder;
+
+public class MediaRecoderTest {
+	private MediaRecorder mMediaRecoder;
+}
